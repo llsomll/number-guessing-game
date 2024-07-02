00967919a36303405e045f5b00fa637be5b68e5a
Dasom Kim <llsomll90@gmail.com>
2024-07-01 20:13:10 -0400

    second commit

--- main.js
+++ main.js
@@ -1,34 +1,94 @@
-//랜덤번호 지정
-//유저가 번호를 입력한다 그리고 go 라는 버튼을 누름
-// 만약에 유저가 랜던번호를 맞추면, 맞췄습니다!
-//랜덤번호가 < 유저번호 down!
-//랜덤번호가 > 유저번호 up!
-//Reset 버튼을 누르면 게임이 리셋된다
-//5번의 기회를 다 쓰면 게임이 끝난다(더이상 추측 불가, 버튼이 disable)
-//유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깎지 않는다.
-//유저가 이미 입력한 숫자를 또 입력하면, 알려준다. 기회를 깎지 않는다.
+// Generate a random number
+// The user enters a number and clicks the "Go" button
+// If the user guesses the random number, display "You guessed it!"
+// If the random number is < user's number, display "Down!"
+// If the random number is > user's number, display "Up!"
+// When the "Reset" button is clicked, the game resets
+// If the user uses up all 3 chances, the game ends (no more guesses allowed, button is disabled)
+// If the user enters a number outside the range of 1~30, inform the user without deducting a chance
+// If the user enters a number they have already guessed, inform the user without deducting a chance
+
 
 let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 3;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
+let questionMark = document.getElementById('question-mark');
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){userInput.value=""});
+
 
 function pickRandomNum() {
-    computerNum = Math.floor(Math.random()*100)+1;
+    computerNum = Math.floor(Math.random()*30)+1;
     console.log("정답: ", computerNum);
 }
 
 pickRandomNum();
 
 function play() {
-    let userValue = userInput.value;
+    let userValue = Number(userInput.value);
+
+    if(userValue < 1 || userValue > 30) {
+        resultArea.textContent = "Enter a number between 1 and 30";
+        return;
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent = "You have already entered this number. Please enter a different number."
+        return;
+    }
+
+    history.push(userValue);
+    console.log(history);
+
+    chances--;
+    chanceArea.textContent = `Remaining chances: ${chances}`;
+    console.log("chance", chances);
+
     if(userValue < computerNum) {
         resultArea.textContent = "UP!!!"
     } else if(userValue > computerNum) {
         resultArea.textContent = "DOWN!!!"
     } else {
-        resultArea.textContent = "YOU GOT IT!!!"
+        resultArea.textContent = "YOU GUESSED IT!!!"
+        gameOver = true;
+        do_animation({ target: questionMark });
     }
+
+
+    if(chances < 1 && userValue !== computerNum){
+        gameOver = true;
+        resultArea.textContent = `FAILED. The number was ${computerNum}`;
+    }
+
+    if(gameOver){
+        playButton.disabled = true;
+    }
+}
+
+function reset() {
+    //user input 창이 깨끗하게 정리되고
+    userInput.value = "";
+    //새로운 번호가 생성되고
+    pickRandomNum();
+    chances = 3;
+    //결과
+    resultArea.textContent = "Result will be displayed here"
+    playButton.disabled = false;
+    gameOver = false;
+    history = [];
+    chanceArea.textContent = `Remaining chances: ${chances}`;
+}
+
+function do_animation(event) {
+    target = event.target;
+    target.classList.remove('zoomSpin');
+    setTimeout( () => {target.classList.add('zoomSpin');}, 0);
 }
